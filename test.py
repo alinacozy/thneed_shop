d1445909c5716e52cfce6007295f267e58e4f95b
alinacozy <alinakoroleva2307@gmail.com>
2023-07-20 20:22:21 +0400

    find order by id работает

--- test.py
+++ test.py
@@ -1,11 +1,10 @@
 import sqlite3
 
+from app import app
 from database_connection import create_tables_if_not_exist, DATABASE_PATH
 from product_queries import Order, find_order_by_id
 
-connection = sqlite3.connect(DATABASE_PATH)
-create_tables_if_not_exist(connection)
-
-a = find_order_by_id(1)
-print (a)
-#print(a.get_ordered_products())
+with app.app_context():
+    a = find_order_by_id(1)
+    print(a.name)
+    #print(a.get_ordered_products())
